8426a3b3093e9a8974a0df19f128d0f2907a34d3
Jonathan <jhii2@outlook.com.au>
2025-05-30 13:46:39 +1000

    Notification bar completed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Notification = () => {
     return (
-        <div>Notification</div>
+        <div
+            className='h-11 bg-blue-500 text-white px-4 flex items-center 
+            justify-center text-center text-sm md:text-base'>
+            Free Delivery for all orders!</div>
     )
 }
 
