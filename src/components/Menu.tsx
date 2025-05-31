b1576a9b7a97c1855c78257b6049b49d58e52f4f
Jonathan <jhii2@outlook.com.au>
2025-06-01 02:02:57 +1000

    finish navbar and footer

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -23,20 +23,20 @@ const Menu = () => {
                 <Image src="/close.png" alt="" width={20} height={20} onClick={() => setOpen(false)} />
             )}
 
-            <div className="bg-blue-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex  flex-col gap-8 items-center text-3xl z-10">
+            {open && <div className="bg-blue-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex  flex-col gap-8 items-center text-3xl z-10">
                 {links.map(item => (
-                    <Link href={item.url} key={item.id}>
+                    <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
                         {item.title}
                     </Link>
                 ))}
                 {!user ? (
-                    <Link href="/login">Login</Link>
+                    <Link href="/login" onClick={() => setOpen(false)}>Login</Link>
                 ) : (
-                    <Link href="/orders">Orders</Link>)}
+                    <Link href="/orders" onClick={() => setOpen(false)}>Orders</Link>)}
                 <Link href="/cart">
                     <CartIcon />
                 </Link>
-            </div>
+            </div>}
         </div>
     )
 }
