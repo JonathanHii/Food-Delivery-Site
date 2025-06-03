3acb45a6c771bbe8d13528e1806abcd7a34e6d96
Jonathan <jhii2@outlook.com.au>
2025-06-03 12:14:21 +1000

    Finish Homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-blue-500 flex items-center justify-between">
+        <div className="h-12 md:h-18 p-4 lg:p-10 xl:p-15 text-blue-500 flex items-center justify-between">
             <Link href="/" className = "font-bold text-xl">FOOOOOD</Link>
             <p>© All Rights Reserved.</p>
         </div>
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Offer = () => {
     return (
-        <div>
-            Offer
+        <div className='bg-black h-screen flex flex-col md:flex-row'>
+            {/* TEXT */}
+            <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
+                <h1 className='text-white text-5xl font-bold xl:text-6xl'>Become a vegan for sure!</h1>
+                <p className='text-white xl:text:xl'>I'm a vegan, but I have to admit, I absolutely love the taste of meat. There's just something about the flavor and texture, especially when it comes to burgers. They're so incredibly satisfying and delicious!</p>
+                <button className='bg-blue-500 text-white rounded-md py-3 px-6'>Order Now</button>
+            </div>
+            {/* IMAGE */}
+            <div className='relative w-full flex-1'>
+                <Image src="/offerProduct.png" alt="" fill className='object-contain' />
+            </div>
         </div>
     )
 }
